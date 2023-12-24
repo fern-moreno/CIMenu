e6cf443aead026fc8297ea7d34a1b2a33ca5f634
fern-moreno <fmoreno5200@gmail.com>
2023-12-23 20:23:11 -0800

    finding pdf files

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello, world!")
-print("test")
+import requests
+from bs4 import BeautifulSoup
+
+menu_url = 'https://uas.csuci.edu/hospitality/hospitality-locations.htm'
+html_text = requests.get(menu_url).text
+soup = BeautifulSoup(html_text, 'html.parser')
+
+for a in soup.find_all('a', href=True):
+     if a['href'].endswith('.pdf'):
+        print(a['href'])
