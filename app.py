3ee0a48af9ab22b518a58dcc20bcff05fbd98e12
fern-moreno <fmoreno5200@gmail.com>
2023-12-23 20:39:59 -0800

    parsing pdf menu

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import requests
+import io
 from bs4 import BeautifulSoup
+from pypdf import PdfReader
 
-menu_url = 'https://uas.csuci.edu/hospitality/hospitality-locations.htm'
-html_text = requests.get(menu_url).text
+csuci_url= 'https://uas.csuci.edu/hospitality/hospitality-locations.htm'
+html_text = requests.get(csuci_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
+pdfs = []
 for a in soup.find_all('a', href=True):
      if a['href'].endswith('.pdf'):
-        print(a['href'])
+        pdfs.append(a['href'])
+
+menu_url = 'https://uas.csuci.edu/' + pdfs[0]
+print(menu_url)
+
+r = requests.get(menu_url)
+f = io.BytesIO(r.content)
+
+reader = PdfReader(f)
+contents = reader.pages[0].extract_text().split('\n')
+print(contents)
+
